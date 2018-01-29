c151b68d9cf5c0bb350b5338171ed672241f35ad
sunnydgoswami <sunnydgoswami@gmail.com>
2018-01-28 19:52:24 -0500

    working normally again

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,24 +1,27 @@
 (function() {
     function config($stateProvider, $locationProvider) {
       $locationProvider
-          .html5Mode({
-              enabled: true,
-              requireBase: false
-          });
+    .html5Mode({
+        enabled: true,
+        requireBase: false
+    });
 
-          $stateProvider
-              .state('landing', {
-                  url: '/',
-                  templateUrl: '/templates/landing.html'
-                })
-                .state('album', {
-                    url: '/album',
-                    templateUrl: '/templates/album.html'
-                })
-                .state('album', {
-                    url: '/album',
-                    templateUrl: '/templates/album.html'
-                });                                          
+    $stateProvider
+      .state('landing', {
+          url: '/',
+          // controller: 'LandingCtrl as landing',
+          templateUrl: '/templates/landing.html'
+        })
+      .state('album', {
+          url: '/album',
+          // controller: 'AlbumCtrl as album',
+          templateUrl: '/templates/album.html'
+      })
+      .state('collection', {
+        url: '/collection',
+        // controller: 'CollectionCtrl as collection',
+        templateUrl: '/templates/collection.html'
+      });
     }
 
     angular
