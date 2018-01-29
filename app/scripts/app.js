42411f3f4cd9bad5dfc49a1dc077d456009c2fd5
sunnydgoswami <sunnydgoswami@gmail.com>
2018-01-29 14:16:37 -0500

    added controllers for landing and collection. Updated the html with angular {{ }} and ng-repeat. used angular.copy for the first time

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,17 +9,17 @@
     $stateProvider
       .state('landing', {
           url: '/',
-          // controller: 'LandingCtrl as landing',
+          controller: 'LandingCtrl as landing',
           templateUrl: '/templates/landing.html'
         })
       .state('album', {
           url: '/album',
-          // controller: 'AlbumCtrl as album',
+          controller: 'AlbumCtrl as album',
           templateUrl: '/templates/album.html'
       })
       .state('collection', {
         url: '/collection',
-        // controller: 'CollectionCtrl as collection',
+        controller: 'CollectionCtrl as collection',
         templateUrl: '/templates/collection.html'
       });
     }
